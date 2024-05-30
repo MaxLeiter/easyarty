8bc273ef98e64402733d8f3edef199e65d24116f
Max Leiter <max.leiter@vercel.com>
2024-05-29 21:01:37 -0700

    fix identifying .ts pages (fixes sitemap)

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  pageExtensions: ['mdx', 'tsx'],
+  pageExtensions: ['mdx', 'tsx', 'ts'],
 };
 
 module.exports = withMDX(nextConfig);
